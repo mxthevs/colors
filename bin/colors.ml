cd6aa254ac19fa5bd57a0279b3143e6095de6c72
Matheus Henrique <matheuscdasilva2@hotmail.com>
2021-12-28 15:28:18 -0300

    feat: Print file contents

--- bin/colors.ml
+++ bin/colors.ml
@@ -4,7 +4,7 @@ let usage = "USAGE: esy x colors <path/to/file>"
 
 let () =
   match Array.to_list Sys.argv with
-  | [ _program; file_name ] -> print_endline file_name
+  | [ _program; file_name ] -> file_name |> File.read |> print_endline
   | _ ->
     prerr_endline usage;
     exit 1
