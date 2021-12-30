9c82a4233e8656e9684a210110cd42721f5daace
Matheus Henrique <matheuscdasilva2@hotmail.com>
2021-12-30 09:57:19 -0300

    feat: Setup Alcotest

---
+++ test/test_runner.ml
@@ -0,0 +1,9 @@
+(**
+  Entrypoint for the test runner.
+  This aggregates all the tests and call Alcotest to run them. When
+  creating a new test suite, don't forget to add it here!
+*)
+
+let () =
+  let open Alcotest in
+  run "Colors" []
