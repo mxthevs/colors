ecaa64fcf9207562611300031eb0fdde4d4b286f
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-01-03 13:43:08 -0300

    feat: Add lsplit2i test

--- test/parser.ml
+++ test/parser.ml
@@ -21,9 +21,17 @@ let cant_split_if_char_is_inexistent () =
 
   (check (option (pair string string))) "should be equal" output (Parser.lsplit2 input ~on:'#')
 
+let can_split_a_string_in_two_inclusive () =
+  let open Alcotest in
+  let input = "I think that #7159C1 is a nice color" in
+  let output = ("I think that ", "#7159C1 is a nice color") in
+
+  (check (pair string string)) "should be equal" output (Option.get (Parser.lsplit2i input ~on:'#'))
+
 let equality =
   [
     ("substrings", `Quick, can_take_substrings);
     ("lsplit2", `Quick, can_split_a_string_in_two);
     ("lsplit2 fail", `Quick, cant_split_if_char_is_inexistent);
+    ("lsplit2i", `Quick, can_split_a_string_in_two_inclusive);
   ]
