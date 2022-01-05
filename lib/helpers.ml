49f01492b83e8ad48dacf37ca28dbe607ff2dc41
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-01-05 15:05:03 -0300

    feat: Create dedup list function

--- lib/helpers.ml
+++ lib/helpers.ml
@@ -1,1 +1,4 @@
 let list_to_option = function x :: _ -> Some x | _ -> None
+let uniq_cons x xs = if List.mem x xs then xs else x :: xs
+let remove_from_right xs = List.fold_right uniq_cons xs []
+let dedup = remove_from_right
