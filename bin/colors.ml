a91045e2b237a068061c16258f0891d81ce2a8ca
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-01-05 15:07:09 -0300

    feat: Read colors from file

--- bin/colors.ml
+++ bin/colors.ml
@@ -2,9 +2,30 @@ open Colors_lib
 
 let usage = "USAGE: esy x colors <path/to/file>"
 
+let handle_file file =
+  let rec aux rest colors =
+    let splitted = Parser.lsplit2i ~on:'#' rest in
+    let hex_size = 7 in
+
+    match splitted with
+    | Some (_, rest) ->
+      let color = String.trim rest |> Parser.take hex_size |> Hex.of_string in
+      aux (Parser.skip hex_size rest) (color :: colors)
+    | None -> colors
+  in
+
+  aux file []
+
 let () =
   match Array.to_list Sys.argv with
-  | [ _program; file_name ] -> file_name |> File.read |> print_endline
+  | [ _program; file_name ] ->
+    file_name
+    |> File.read
+    |> handle_file
+    |> List.map Hex.to_string
+    |> List.filter (fun c -> not (Hex.is_nocolor c))
+    |> Helpers.dedup
+    |> List.iter print_endline
   | _ ->
     prerr_endline usage;
     exit 1
