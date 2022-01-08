d8f6bf43aac2379ed5c6760c2e864228b08449f3
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-01-08 10:40:34 -0300

    feat: Manage RGB values

--- lib/rgb.ml
+++ lib/rgb.ml
@@ -1,3 +1,28 @@
-type t =
+type rgb =
   | RGB  of int * int * int
   | RGBa of int * int * int * float
+
+type t = rgb option
+
+let from_string hex_s =
+  try
+    let r = hex_s |> Parser.skip 1 |> Parser.take 2 |> Helpers.cat "0x" |> int_of_string in
+    let g = hex_s |> Parser.skip 3 |> Parser.take 2 |> Helpers.cat "0x" |> int_of_string in
+    let b = hex_s |> Parser.skip 5 |> Parser.take 2 |> Helpers.cat "0x" |> int_of_string in
+
+    if (r >= 0 && r <= 255) && (g >= 0 && g <= 255) && b >= 0 && b <= 255 then
+      Some (RGB (r, g, b))
+    else
+      None
+  with
+  | Failure _ -> None
+
+let from_hex hex =
+  match hex with
+  | Some hex_s -> from_string hex_s
+  | None -> None
+
+let to_string = function
+  | Some (RGB (r, g, b)) -> Printf.sprintf "rgb(%d, %d, %d)" r g b
+  | Some (RGBa (r, g, b, a)) -> Printf.sprintf "rgba(%d, %d, %d, %f)" r g b a
+  | None -> "[NO_COLOR]"
--- lib/rgb.mli
+++ lib/rgb.mli
@@ -1,3 +1,11 @@
-type t =
+type rgb =
   | RGB  of int * int * int
   | RGBa of int * int * int * float
+
+type t = rgb option
+
+val from_string : string -> t
+
+val from_hex : Hex.t -> t
+
+val to_string : t -> string
