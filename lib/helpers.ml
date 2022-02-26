4c9b431043bd1d1d95f3d6474515b7ef34527225
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-02-26 12:50:15 -0300

    feat: `to_fixed` helper function

--- lib/helpers.ml
+++ lib/helpers.ml
@@ -6,3 +6,13 @@ let uniq_cons x xs = if List.mem x xs then xs else x :: xs
 let remove_from_right xs = List.fold_right uniq_cons xs []
 let dedup = remove_from_right
 let cat s1 s2 = s1 ^ s2
+let round x = floor (x +. 0.5)
+
+let round_dfrac d x =
+  if x -. round x = 0. then
+    x
+  else
+    let m = 10. ** float d in
+    floor ((x *. m) +. 0.5) /. m
+
+let to_fixed = round_dfrac
