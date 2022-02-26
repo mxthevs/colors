430c818aa01b180da1d4409cd795e824ba68f0dc
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-02-26 12:53:19 -0300

    feat: Parse RGBa values

--- lib/rgb.ml
+++ lib/rgb.ml
@@ -4,15 +4,37 @@ type rgb =
 
 type t = rgb option
 
+let is_valid_rgb (r, g, b) ~alpha =
+  (r >= 0 && r <= 255)
+  && (g >= 0 && g <= 255)
+  && (b >= 0 && b <= 255)
+  && alpha >= 1.0
+  && alpha <= 255.0
+
 let from_hex_string hex_s =
   try
-    (* TODO: Parsing of RGBa values is not implemented yet *)
     let r = hex_s |> Parser.skip 1 |> Parser.take 2 |> Helpers.cat "0x" |> int_of_string in
     let g = hex_s |> Parser.skip 3 |> Parser.take 2 |> Helpers.cat "0x" |> int_of_string in
     let b = hex_s |> Parser.skip 5 |> Parser.take 2 |> Helpers.cat "0x" |> int_of_string in
 
-    if (r >= 0 && r <= 255) && (g >= 0 && g <= 255) && b >= 0 && b <= 255 then
-      Some (RGB (r, g, b))
+    if String.length hex_s = 7 then
+      match is_valid_rgb (r, g, b) ~alpha:1.0 with
+      | true -> Some (RGB (r, g, b))
+      | false -> None
+    else if String.length hex_s = 9 then
+      let alpha =
+        hex_s
+        |> Parser.skip 7
+        |> Parser.take 2
+        |> Helpers.cat "0x"
+        |> float_of_string
+        |> (fun a -> a /. 255.)
+        |> Helpers.to_fixed 3
+      in
+
+      match is_valid_rgb (r, g, b) ~alpha with
+      | true -> Some (RGBa (r, g, b, alpha))
+      | false -> None
     else
       None
   with
