2deb4ecf14ebb14ada61be920164d770ebaa5183
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-03-05 15:26:28 -0300

    feat: Colorize messages

--- bin/colors.ml
+++ bin/colors.ml
@@ -14,8 +14,8 @@ let handle_file file =
 
   List.rev (aux file [])
 
-let get_color_string show_rgb hex =
-  if not show_rgb then
+let get_color_string hex ~with_rgb =
+  if not with_rgb then
     hex
   else
     let rgb = Rgb.to_string (Rgb.from_hex_string hex) in
@@ -25,7 +25,7 @@ let run file show_rgb =
   let file_name =
     match file with
     | Some file -> file
-    | None -> failwith "No file path provided"
+    | None -> failwith (Ui.error "No file path provided!")
   in
 
   let colors =
@@ -35,21 +35,27 @@ let run file show_rgb =
     |> List.filter Hex.is_valid
     |> List.map Hex.to_string
     |> Helpers.dedup
-    |> List.map (get_color_string show_rgb)
+    |> List.map (get_color_string ~with_rgb:show_rgb)
   in
 
   match colors with
-  | [] -> print_endline "No colors found in this file."
-  | colors -> List.iter print_endline colors
+  | [] -> print_endline (Ui.warn "No colors found in this file.")
+  | colors ->
+    print_endline (Ui.success "Found the following colors in this file:");
+    List.iter print_endline colors
 
 let safe_run file show_rgb =
   try run file show_rgb with
   | Failure message ->
-    Printf.eprintf "Error: %s!\n" message;
+    Printf.eprintf "%s\n" message;
     exit 1
   | error ->
+    print_string (Ui.error "Oops! An unhandled error occured!");
+    print_endline "If you can, please report this bug.";
+    print_endline (Ui.info "https://github.com/mxthevs/colors/issues");
+
     let trace = Printexc.get_backtrace () in
-    Printf.eprintf "Error: exception %s\n%s" (Printexc.to_string error) trace
+    Printf.eprintf "Exception %s\n%s" (Printexc.to_string error) trace
 
 let main () =
   Printexc.record_backtrace true;
---
+++ lib/ui.ml
@@ -0,0 +1,4 @@
+let success = Printf.sprintf "\n✅\x1b[0;32m %s\x1b[0m\n"
+let info = Printf.sprintf "\nℹ️ \x1b[0;34m %s\x1b[0m\n"
+let warn = Printf.sprintf "\n⚠️ \x1b[0;33m %s\x1b[0m\n"
+let error = Printf.sprintf "\n😵\x1b[0;31m %s\x1b[0m\n"
